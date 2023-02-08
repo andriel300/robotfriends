2881edcf6f32a7570c30c1d5a884e269d635d8b3
andriel300 <andrielscript@gmail.com>
2023-02-08 20:34:45 -0300

    updated packages and clean the code and installed axios and prop-types again

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Card = ({ name, email, id }) => {
+function Card({ name, email, id }) {
   return (
     <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
       <img alt="robots" src={`https://robohash.org/${id}?size=200x200`} />
@@ -11,9 +11,9 @@ const Card = ({ name, email, id }) => {
       </div>
     </div>
   );
-};
+}
 
-Card.PropTypes = {
+Card.propTypes = {
   email: PropTypes.string.isRequired,
   id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
