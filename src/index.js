8d7c80da48a564de8a82e3a96591883bb257eaf8
andriel300 <andrielscript@gmail.com>
2023-02-12 12:30:05 -0300

    refacotr actions, constants, reducers and add Error Boundary components again, fix problems related to redux and redux tools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from "react";
-// import ReactDOM from 'react-dom'; The new way to import createRoot:
+import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./containers/App";
+import { searchRobots, requestRobots } from "./reducers";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
 import "tachyons";
-// import registerServiceWorker from './registerServiceWorker';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+const logger = createLogger();
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat([thunkMiddleware, logger]),
+});
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
